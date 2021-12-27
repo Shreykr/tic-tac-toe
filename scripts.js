de983feffa1cbb2b6ceea8efa444c3c980b24f29
Shreyas K R <shreyaskr1022@gmail.com>
2021-12-28 04:56:27 +0530

    Add game logic - In progress

--- scripts.js
+++ scripts.js
@@ -12,24 +12,74 @@ const resetButtonAnimationHandler = () => {
 };
 
 const handleTicTacToe = () => {
+  let boxes = document.querySelectorAll(".box");
+  let reset = document.getElementById("reset");
+  let score1 = document.getElementById("score-1");
+  let score2 = document.getElementById("score-2");
   let boxValue = "X";
+  let tracker = new Array(9);
+
+  const updatePoints = (winnerVal) => {
+    if (winnerVal === "X") {
+      score1.innerHTML = Number(score1.innerHTML) + 1;
+    } else if (winnerVal === "O") {
+      score2.innerHTML = Number(score1.innerHTML) + 1;
+    }
+  };
+  const checkWinner = () => {
+    console.log(tracker);
+    if (
+      tracker[0] !== undefined &&
+      tracker[0] === tracker[1] &&
+      tracker[0] === tracker[2]
+    ) {
+      updatePoints(tracker[0]);
+      Swal.fire({
+        icon: "success",
+        title: "Win",
+        timer: "2250",
+        showConfirmButton: false,
+      });
+    }
+  };
 
   const attachBoxEventListener = (target) => {
     target.forEach((box, index) =>
       box.addEventListener("click", (event) => {
-        if (box.innerHTML === "") {
+        if (!box.innerHTML) {
           box.innerHTML = boxValue;
+          tracker[index] = boxValue;
+          checkWinner();
+          boxValue === "X" ? (boxValue = "O") : (boxValue = "X");
         } else {
-          Swal.fire("Box already filled", "", "error");
+          Swal.fire({
+            icon: "error",
+            title: "Box already filled",
+            timer: "2250",
+            showConfirmButton: false,
+          });
         }
       })
     );
   };
 
-  let boxes = document.querySelectorAll(".box");
-  console.log(boxes);
+  const resetAll = () => {
+    score1.innerHTML = 0;
+    score2.innerHTML = 0;
+    boxes.forEach((box, index) => {
+      box.innerHTML = "";
+    });
+    tracker = new Array(9);
+  };
+
+  const attachResetHandler = (target) => {
+    target.addEventListener("click", (event) => {
+      resetAll();
+    });
+  };
+
   attachBoxEventListener(boxes);
-  let reset = document.getElementById("reset");
+  attachResetHandler(reset);
 };
 
 resetButtonAnimationHandler();
